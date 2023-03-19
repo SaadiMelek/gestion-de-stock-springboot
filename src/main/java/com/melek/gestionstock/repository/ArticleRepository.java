4148bfcc4d4c35ef2f77260aaf4344d43e03ee91
saadi.melek <me.saadi.ext@axione.fr>
2023-03-19 15:49:23 +0100

    Developper les exceptions personnalisees et le gestionnaire global d'exception

--- src/main/java/com/melek/gestionstock/repository/ArticleRepository.java
+++ src/main/java/com/melek/gestionstock/repository/ArticleRepository.java
@@ -11,13 +11,13 @@ import java.util.List;
 @Repository
 public interface ArticleRepository extends JpaRepository<Article, Integer> {
 
-    /*Article findByCodeArticle(String code);
+    Article findByCodeArticle(String code);
 
-    @Query("select a from Article a where codeArticle=:code and designation=:designation")
+    @Query("select a from Article a where a.codeArticle=:code and a.designation=:designation")
     List<Article> findByCustomJpqlQuery(@Param("code") String c, String designation);
 
     @Query(value = "select * from article where codeArticle = :code", nativeQuery = true)
     List<Article> findByCustomNativeQuery(@Param("code") String c);
 
-    List<Article> findByCodeArticleIgnoreCaseAndDesignationIgnoreCase(String codeArticle, String designation);*/
+    List<Article> findByCodeArticleIgnoreCaseAndDesignationIgnoreCase(String codeArticle, String designation);
 }
